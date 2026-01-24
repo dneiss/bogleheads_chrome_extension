dfb3e0ed796d8311d926fa0d81e25d8d1a17a17e
David Neiss <davidaneiss@gmail.com>
2026-01-24 10:25:08 -0500

    Found an issue in the extension that was causing error on load

--- extension/background.js
+++ extension/background.js
@@ -14,18 +14,3 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   }
 });
 
-// Auto-open side panel when navigating to bogleheads.org
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url && tab.url.includes('bogleheads.org')) {
-    chrome.sidePanel.open({ tabId: tabId });
-  }
-});
-
-// Also open when a bogleheads.org tab becomes active
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tab) {
-    if (tab.url && tab.url.includes('bogleheads.org')) {
-      chrome.sidePanel.open({ tabId: activeInfo.tabId });
-    }
-  });
-});
